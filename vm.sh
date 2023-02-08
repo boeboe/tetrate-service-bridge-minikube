3d1c7b83ed1101894911fffe8ee763c8f791e062
Bart Van Bos <bart@tetrate.io>
2023-02-08 17:28:29 +0100

    patch metallb

--- vm.sh
+++ vm.sh
@@ -62,21 +62,21 @@ if [[ ${ACTION} = "on-minikube-host" ]]; then
   export KUBECTL=$(which kubectl)
   envsubst < ./config/tsb-gui-template.service > ./config/tsb-gui.service ;
   sudo cp ./config/tsb-gui.service /etc/systemd/system ;
-  if systemctl is-active tsb-gui.service &>/dev/null ; then sudo systemctl stop tsb-gui && sudo systemctl daemon-reload ; fi
+  if systemctl is-active tsb-gui.service &>/dev/null ; then sudo systemctl stop tsb-gui 2>/dev/null && sudo systemctl daemon-reload ; fi
   sudo systemctl enable tsb-gui ;
   sudo systemctl start tsb-gui ;
   sleep 3
 
   envsubst < ./config/vm-gateway-template.service > ./config/vm-gateway.service ;
   sudo cp ./config/vm-gateway.service /etc/systemd/system ;
-  if systemctl is-active vm-gateway.service &>/dev/null ; then sudo systemctl stop vm-gateway && sudo systemctl daemon-reload ; fi
+  if systemctl is-active vm-gateway.service &>/dev/null ; then sudo systemctl stop vm-gateway 2>/dev/null && sudo systemctl daemon-reload ; fi
   sudo systemctl enable vm-gateway ;
   sudo systemctl start vm-gateway ;
   sleep 3
 
   envsubst < ./config/vm-repo-template.service > ./config/vm-repo.service ;
   sudo cp ./config/vm-repo.service /etc/systemd/system ;
-  if systemctl is-active vm-repo.service &>/dev/null ; then sudo systemctl stop vm-repo && sudo systemctl daemon-reload ; fi
+  if systemctl is-active vm-repo.service &>/dev/null ; then sudo systemctl stop vm-repo 2>/dev/null && sudo systemctl daemon-reload ; fi
   sudo systemctl enable vm-repo ;
   sudo systemctl start vm-repo ;
 
