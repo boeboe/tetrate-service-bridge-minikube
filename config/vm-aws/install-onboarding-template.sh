5fef5f8e4113e64f17a0dbeb7c0701f3461deb54
Bart Van Bos <bart@tetrate.io>
2023-02-08 13:00:59 +0100

    expose both vm gw and vm package repo

--- config/vm-aws/install-onboarding-template.sh
+++ config/vm-aws/install-onboarding-template.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-# Download and install the onboarding packages
-curl -k -fL -o /tmp/onboarding-agent.deb "https://vm-onboarding.tetrate.prod/install/deb/amd64/onboarding-agent.deb" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
-curl -k -fL -o /tmp/istio-sidecar.deb "https://vm-onboarding.tetrate.prod/install/deb/amd64/istio-sidecar.deb" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
-apt-get install -o Dpkg::Options::="--force-confold" -y /tmp/onboarding-agent.deb
-apt-get install -o Dpkg::Options::="--force-confold" -y /tmp/istio-sidecar.deb
-rm /tmp/onboarding-agent.deb
-rm /tmp/istio-sidecar.deb
+if which apt-get &>/dev/null ; then 
+# Download and install the onboarding packages (DEB)
+  curl -k -fL -o /tmp/onboarding-agent.deb "https://vm-onboarding.tetrate.prod/install/deb/amd64/onboarding-agent.deb" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
+  curl -k -fL -o /tmp/istio-sidecar.deb "https://vm-onboarding.tetrate.prod/install/deb/amd64/istio-sidecar.deb" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
+  apt-get install -o Dpkg::Options::="--force-confold" -y /tmp/onboarding-agent.deb
+  apt-get install -o Dpkg::Options::="--force-confold" -y /tmp/istio-sidecar.deb
+  rm /tmp/onboarding-agent.deb
+  rm /tmp/istio-sidecar.deb
+else
+  # Download and install the onboarding packages (RPM)
+  curl -k -fL -o /tmp/onboarding-agent.rpm "https://vm-onboarding.tetrate.prod/install/rpm/amd64/onboarding-agent.rpm" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
+  curl -k -fL -o /tmp/istio-sidecar.rpm "https://vm-onboarding.tetrate.prod/install/rpm/amd64/istio-sidecar.rpm" --resolve "vm-onboarding.tetrate.prod:443:__TSB_VM_ONBOARDING_ENDPOINT__"
+  rpm -i /tmp/onboarding-agent.rpm
+  rpm -i /tmp/istio-sidecar.rpm
+  rm /tmp/onboarding-agent.rpm
+  rm /tmp/istio-sidecar.rpm
+fi
 
 # Allow the Envoy sidecar to bind privileged ports, such as port 80 (needed for the obs-tester egress)
 setcap CAP_NET_BIND_SERVICE=+eip /usr/local/bin/envoy
