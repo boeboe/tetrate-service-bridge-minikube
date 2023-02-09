986de52e96780ca28617e70cb5ed5fa81ca7a125
Bart Van Bos <bart@tetrate.io>
2023-02-09 12:39:15 +0100

    WIP

--- minikube.sh
+++ minikube.sh
@@ -4,36 +4,34 @@ ACTION=${1}
 K8S_VERSION=${2}
 
 # Network configuration
-#   MINIKUBE_NETWORK        : name of the created docker bridge network
+#   DOCKER_NETWORK        : name of the created docker bridge network
 #   AWS_SECONDARY_SUBNET    : secondary aws subnet to be configured in your VPC
-#   MINIKUBE_SUBNET         : minikube subnet (ip addresses for k8s api servers)
 #   METALLB_MGMT_SUBNET     : metallb subnet for k8s service lb ip assignment within mgmt cluster
 #   METALLB_ACTIVE_SUBNET   : metallb subnet for k8s service lb ip assignment within active cluster
 #   METALLB_STANDBY_SUBNET  : metallb subnet for k8s service lb ip assignment within standby cluster
 #
 #   Make sure that
-#     (1) MINIKUBE_SUBNET and METALLB_XYZ_SUBNET do not overlap
-#     (2) MINIKUBE_SUBNET and METALLB_XYZ_SUBNET fall within AWS_SECONDARY_SUBNET
+#     (1) METALLB_XYZ_SUBNET do not overlap
+#     (2) METALLB_XYZ_SUBNET fall within AWS_SECONDARY_SUBNET
 #   Check https://www.ipaddressguide.com/cidr
 
-MINIKUBE_NETWORK=tsb-demo
+DOCKER_NETWORK=tsb-demo
 AWS_SECONDARY_SUBNET=172.10.16.0/20     # 172.10.16.0 - 172.10.31.255
-MINIKUBE_SUBNET=172.10.16.0/22          # 172.10.16.0 - 172.10.19.255
-METALLB_MGMT_SUBNET=172.10.20.0/22      # 172.10.20.0 - 172.10.23.255
-METALLB_ACTIVE_SUBNET=172.10.24.0/22    # 172.10.24.0 - 172.10.27.255
-METALLB_STANDBY_SUBNET=172.10.28.0/22   # 172.10.28.0 - 172.10.31.255
+MGMT_METALLB_SUBNET=172.10.16.0/22          # 172.10.16.0 - 172.10.19.255
+ACTIVE_METALLB_SUBNET=172.10.20.0/22      # 172.10.20.0 - 172.10.23.255
+STANDBY_METALLB_SUBNET=172.10.24.0/22    # 172.10.24.0 - 172.10.27.255
 
-MGMT_CLUSTER_PROFILE=mgmt-cluster-m1
-MGMT_CLUSTER_METALLB_STARTIP=172.10.20.1
-MGMT_CLUSTER_METALLB_ENDIP=172.10.23.254
+MGMT_PROFILE=mgmt-cluster-m1
+MGMT_METALLB_STARTIP=172.10.16.1
+MGMT_METALLB_ENDIP=172.10.19.254
 
-ACTIVE_CLUSTER_PROFILE=active-cluster-m2
-ACTIVE_CLUSTER_METALLB_STARTIP=172.10.24.1
-ACTIVE_CLUSTER_METALLB_ENDIP=172.10.27.254
+ACTIVE_PROFILE=active-cluster-m2
+ACTIVE_METALLB_STARTIP=172.10.20.1
+ACTIVE_METALLB_ENDIP=172.10.23.254
 
-STANDBY_CLUSTER_PROFILE=standby-cluster-m3
-STANDBY_CLUSTER_METALLB_STARTIP=172.10.28.1
-STANDBY_CLUSTER_METALLB_ENDIP=172.10.31.254
+STANDBY_PROFILE=standby-cluster-m3
+STANDBY_METALLB_STARTIP=172.10.24.1
+STANDBY_METALLB_ENDIP=172.10.27.254
 
 # Configure metallb start and end IP
 #   args:
@@ -96,56 +94,51 @@ function load_images {
 }
 
 if [[ ${ACTION} = "up" ]]; then
-  
-  if docker network inspect ${MINIKUBE_NETWORK} | grep ${AWS_SECONDARY_SUBNET} ; then
-    echo "Docker network ${MINIKUBE_NETWORK} matches the expected aws subnet ${AWS_SECONDARY_SUBNET}"
-  else
-    docker network rm ${MINIKUBE_NETWORK} &>/dev/null ;
-    docker network create ${MINIKUBE_NETWORK} --subnet ${AWS_SECONDARY_SUBNET} ;
-
-    if ! docker network inspect ${MINIKUBE_NETWORK} | grep ${AWS_SECONDARY_SUBNET} ; then
-      echo "Failed to create docker network ${MINIKUBE_NETWORK} with subnet ${AWS_SECONDARY_SUBNET}"
-      echo "Check manually what goes wrong"
-      exit 1
-    else
-      echo "Docker network ${MINIKUBE_NETWORK} matches the expected aws subnet ${AWS_SECONDARY_SUBNET}"
-    fi
-  fi
 
   # Start minikube profiles for all clusters
-  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${MGMT_CLUSTER_PROFILE} --network ${MINIKUBE_NETWORK} --subnet ${MINIKUBE_SUBNET} ;
-  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${ACTIVE_CLUSTER_PROFILE} --network ${MINIKUBE_NETWORK} --subnet ${MINIKUBE_SUBNET} ;
-  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${STANDBY_CLUSTER_PROFILE} --network ${MINIKUBE_NETWORK} --subnet ${MINIKUBE_SUBNET} ;
+  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${MGMT_PROFILE} --network ${DOCKER_NETWORK} ;
+  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${ACTIVE_PROFILE} --network ${DOCKER_NETWORK} ;
+  minikube start --kubernetes-version=v${K8S_VERSION} --profile ${STANDBY_PROFILE} --network ${DOCKER_NETWORK} ;
+
+  # Configure network routing for metallb ranges to minikube
+  DOCKER_NETWORK_ID=$(docker network inspect ${} --format '{{.Id}}'  | cut --characters -5)
+  DOCKER_INTF=$(ip link show | grep ${DOCKER_NETWORK_ID} | head -n1 | cut -d ':' -f2 | tr -d " ")
+  MGMT_MINIKUBE_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cl1)
+  ACTIVE_MINIKUBE_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cl2-m2)
+  STANDBY_MINIKUBE_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cl3-m3)
+  sudo -E ip route add ${MGMT_METALLB_SUBNET} via ${MGMT_MINIKUBE_IP} dev ${DOCKER_INTF}
+  sudo -E ip route add ${ACTIVE_METALLB_SUBNET} via ${ACTIVE_MINIKUBE_IP} dev ${DOCKER_INTF}
+  sudo -E ip route add ${STANDBY_METALLB_SUBNET} via ${STANDBY_MINIKUBE_IP} dev ${DOCKER_INTF}
 
   # Configure and enable metallb in all clusters
-  configure_metallb ${MGMT_CLUSTER_PROFILE} ${MGMT_CLUSTER_METALLB_STARTIP} ${MGMT_CLUSTER_METALLB_ENDIP} ;
-  configure_metallb ${ACTIVE_CLUSTER_PROFILE} ${ACTIVE_CLUSTER_METALLB_STARTIP} ${ACTIVE_CLUSTER_METALLB_ENDIP} ;
-  configure_metallb ${STANDBY_CLUSTER_PROFILE} ${STANDBY_CLUSTER_METALLB_STARTIP} ${STANDBY_CLUSTER_METALLB_ENDIP} ;
+  configure_metallb ${MGMT_PROFILE} ${MGMT_METALLB_STARTIP} ${MGMT_METALLB_ENDIP} ;
+  configure_metallb ${ACTIVE_PROFILE} ${ACTIVE_METALLB_STARTIP} ${ACTIVE_METALLB_ENDIP} ;
+  configure_metallb ${STANDBY_PROFILE} ${STANDBY_METALLB_STARTIP} ${STANDBY_METALLB_ENDIP} ;
 
-  minikube --profile ${MGMT_CLUSTER_PROFILE} addons enable metallb ;
-  minikube --profile ${ACTIVE_CLUSTER_PROFILE} addons enable metallb ;
-  minikube --profile ${STANDBY_CLUSTER_PROFILE} addons enable metallb ;
+  minikube --profile ${MGMT_PROFILE} addons enable metallb ;
+  minikube --profile ${ACTIVE_PROFILE} addons enable metallb ;
+  minikube --profile ${STANDBY_PROFILE} addons enable metallb ;
 
   # Pull images locally and sync them to minikube profiles
   sync_images ;
-  load_images ${MGMT_CLUSTER_PROFILE} &
+  load_images ${MGMT_PROFILE} &
   pid_load_images_mgmt_cluster=$!
-  load_images ${ACTIVE_CLUSTER_PROFILE} &
+  load_images ${ACTIVE_PROFILE} &
   pid_load_images_active_cluster=$!
-  load_images ${STANDBY_CLUSTER_PROFILE} &
+  load_images ${STANDBY_PROFILE} &
   pid_load_images_standby_cluster=$!
   wait $pid_load_images_mgmt_cluster
   wait $pid_load_images_active_cluster
   wait $pid_load_images_standby_cluster
 
   # Add nodes labels for locality based routing (region and zone)
-  kubectl --context ${MGMT_CLUSTER_PROFILE} label node ${MGMT_CLUSTER_PROFILE} topology.kubernetes.io/region=region1 --overwrite=true ;
-  kubectl --context ${ACTIVE_CLUSTER_PROFILE} label node ${ACTIVE_CLUSTER_PROFILE} topology.kubernetes.io/region=region1 --overwrite=true ;
-  kubectl --context ${STANDBY_CLUSTER_PROFILE} label node ${STANDBY_CLUSTER_PROFILE} topology.kubernetes.io/region=region2 --overwrite=true ;
+  kubectl --context ${MGMT_PROFILE} label node ${MGMT_PROFILE} topology.kubernetes.io/region=region1 --overwrite=true ;
+  kubectl --context ${ACTIVE_PROFILE} label node ${ACTIVE_PROFILE} topology.kubernetes.io/region=region1 --overwrite=true ;
+  kubectl --context ${STANDBY_PROFILE} label node ${STANDBY_PROFILE} topology.kubernetes.io/region=region2 --overwrite=true ;
 
-  kubectl --context ${MGMT_CLUSTER_PROFILE} label node ${MGMT_CLUSTER_PROFILE} topology.kubernetes.io/zone=zone1a --overwrite=true ;
-  kubectl --context ${ACTIVE_CLUSTER_PROFILE} label node ${ACTIVE_CLUSTER_PROFILE} topology.kubernetes.io/zone=zone1b --overwrite=true ;
-  kubectl --context ${STANDBY_CLUSTER_PROFILE} label node ${STANDBY_CLUSTER_PROFILE} topology.kubernetes.io/zone=zone2a --overwrite=true ;
+  kubectl --context ${MGMT_PROFILE} label node ${MGMT_PROFILE} topology.kubernetes.io/zone=zone1a --overwrite=true ;
+  kubectl --context ${ACTIVE_PROFILE} label node ${ACTIVE_PROFILE} topology.kubernetes.io/zone=zone1b --overwrite=true ;
+  kubectl --context ${STANDBY_PROFILE} label node ${STANDBY_PROFILE} topology.kubernetes.io/zone=zone2a --overwrite=true ;
 
   exit 0
 fi
@@ -153,13 +146,13 @@ fi
 if [[ ${ACTION} = "down" ]]; then
 
   # Stop and delete minikube profiles
-  minikube stop --profile ${MGMT_CLUSTER_PROFILE} ;
-  minikube stop --profile ${ACTIVE_CLUSTER_PROFILE} ;
-  minikube stop --profile ${STANDBY_CLUSTER_PROFILE} ;
+  minikube stop --profile ${MGMT_PROFILE} ;
+  minikube stop --profile ${ACTIVE_PROFILE} ;
+  minikube stop --profile ${STANDBY_PROFILE} ;
 
-  minikube delete --profile ${MGMT_CLUSTER_PROFILE} ;
-  minikube delete --profile ${ACTIVE_CLUSTER_PROFILE} ;
-  minikube delete --profile ${STANDBY_CLUSTER_PROFILE} ;
+  minikube delete --profile ${MGMT_PROFILE} ;
+  minikube delete --profile ${ACTIVE_PROFILE} ;
+  minikube delete --profile ${STANDBY_PROFILE} ;
 
   exit 0
 fi
